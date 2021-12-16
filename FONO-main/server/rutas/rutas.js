ccaa630d0f7d6006ad1bd1f0b7f46c3841287392
Fred <fred.cruz193@tectijuana.edu.mx>
2021-12-16 02:02:40 -0800

    Redireccionando

--- FONO-main/server/rutas/rutas.js
+++ FONO-main/server/rutas/rutas.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req,res) => {
-	res.redirect('cliente.html');
+	res.redirect('index3.html');
 })
 
 module.exports = router;
