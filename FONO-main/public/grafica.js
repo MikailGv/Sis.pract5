c95fd9758a19b62b47f8dc9128a730530a150ee7
Fred <fred.cruz193@tectijuana.edu.mx>
2021-12-15 23:33:18 -0800

    Se agrega el procesamiento del recibir y simplifica html del oyente

---
+++ FONO-main/public/grafica.js
@@ -0,0 +1,20 @@
+//Codigo para recibir sockets
+var socket = io.connect('http://localhost:5001', {'forceNew': true});
+   //graficado
+    var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple'
+    });
+
+   socket.on('voice', function(arrayBuffer) {
+
+	   console.log("A llegado el audio");
+	   var blob = new Blob([arrayBuffer], { 'type' : 'audio/ogg; codecs=opus' });
+	   var audio = document.createElement('audio');
+	   audio.src = window.URL.createObjectURL(blob);
+
+	   audio.play(); 
+	   wavesurfer.loadBlob(blob);//cargando audio
+  }); 
+    
